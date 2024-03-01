24337418f86fd615a4ff21b9bbbeb98c5f83c92b
Herobroto <herobrotelpro@gmail.com>
2024-03-01 09:54:29 -0600

    #1 Finalizado con Socket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,172 @@
-import Image from "next/image";
+"use client";
+
+import "./globals.css";
+import { useState, useEffect } from "react";
+
+const inputStylesClass = "bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [badgeNumber, setBadgeNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
+  const [namePost, setNamePost] = useState("Esperando datos...");
+  const [lastNamePost, setLastNamePost] = useState("Esperando datos...");
+  const [badgeNumberPost, setBadgeNumberPost] = useState("Esperando datos...");
+  const [passwordPost, setPasswordPost] = useState("");
+  const [rolePost, setRolePost] = useState("Esperando datos...");
+
+  useEffect(() => {
+    const wsUrl = process.env.NEXT_PUBLIC_WS_URL!;
+    console.log(wsUrl)
+    const socket = new WebSocket(wsUrl); 
+
+    socket.onmessage = (event) => {
+      console.log('Recibido de WebSocket:', event.data);
+      const data = JSON.parse(event.data);
+      setNamePost(data.name);
+      setLastNamePost(data.lastName);
+      setBadgeNumberPost(data.badgeNumber);
+      setPasswordPost(data.password);
+      setRolePost(data.role);
+    };
+
+ 
+    socket.addEventListener('open', (event) => {
+      console.log('Conectado al servidor WS');
+      console.log(event);
+    });
+
+
+    socket.addEventListener('error', (event) => {
+      console.error('WebSocket error:', event);
+    });
+
+  
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  const sendMessageToFirstApi = async (): Promise<void> => {
+    try {
+      const user = { name: name, lastName: lastName, badgeNumber: badgeNumber, password: password, role: role };
+      console.log(JSON.stringify(user))
+      const response = await fetch(process.env.NEXT_PUBLIC_API_URL! + 'users', { 
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      });
+      console.log(response)
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Mensaje enviado a la API y recibido respuesta...', data);
+      } else {
+        console.error('Error al enviar mensaje:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error al enviar mensaje a la API:', error);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="flex flex-col items-center justify-center">
+      <div id="formContainer" className="flex min-h-screen flex-col items-center justify-center">
+        <div id="form" className="flex flex-col gap-4">
+          <label>
+            Nombre: <br />
+            <input className={inputStylesClass} autoComplete="no"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
-          </a>
+          </label>
+          <label>
+            Apellidos: <br />
+            <input className={inputStylesClass} autoComplete="no"
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </label>
+          <label>
+            Matrícula: <br />
+            <input className={inputStylesClass} autoComplete="no"
+              type="text"
+              value={badgeNumber}
+              onChange={(e) => setBadgeNumber(e.target.value)}
+            />
+          </label>
+          <label>
+            Contraseña: <br />
+            <input className={inputStylesClass} autoComplete="no"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <label>
+            Rol: <br />
+            <input className={inputStylesClass} autoComplete="no"
+              type="text"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+            />
+          </label>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => sendMessageToFirstApi()}>Registrarse</button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <div id="result" className="flex flex-col items-center justify-center gap-4 bg-gray-800">
+        <label>
+            Nombre: <br />
+            <input className={inputStylesClass} autoComplete="no"
+              type="text"
+              value={namePost}
+              onChange={(e) => setName(e.target.value)}
+              disabled={true}
+            />
+          </label>
+          <label>
+            Apellidos: <br />
+            <input className={inputStylesClass} autoComplete="no"
+              type="text"
+              value={lastNamePost}
+              onChange={(e) => setLastName(e.target.value)}
+              disabled={true}
+            />
+          </label>
+          <label>
+            Matrícula: <br />
+            <input className={inputStylesClass} autoComplete="no"
+              type="text"
+              value={badgeNumberPost}
+              onChange={(e) => setBadgeNumber(e.target.value)}
+              disabled={true}
+            />
+          </label>
+          <label>
+            Contraseña: <br />
+            <input className={inputStylesClass} autoComplete="no"
+              type="password"
+              value={passwordPost}
+              onChange={(e) => setPassword(e.target.value)}
+              disabled={true}
+            />
+          </label>
+          <label>
+            Rol: <br />
+            <input className={inputStylesClass} autoComplete="no"
+              type="text"
+              value={rolePost}
+              onChange={(e) => setRole(e.target.value)}
+              disabled={true}
+            />
+          </label>
+        </div>
     </main>
   );
 }
